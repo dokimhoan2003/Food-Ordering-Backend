7087c0cc304da0a55aaa63e337c70c7566a0b717
Do Kim Hoan <kimhoando2003@gmail.com>
2024-08-22 22:51:06 +0700

    22/08

---
+++ src/main/java/com/hoan/FoodOrdering/model/User.java
@@ -0,0 +1,36 @@
+package com.hoan.FoodOrdering.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.hoan.FoodOrdering.dto.RestaurantDto;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String fullName;
+
+    private String email;
+
+    private String password;
+
+    private USER_ROLE role;
+
+    @JsonIgnore
+    @OneToMany
+    private List<Order> orders = new ArrayList<>();
+
+    @ElementCollection
+    private List<RestaurantDto> favorites = new ArrayList<>();
+}
